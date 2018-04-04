ecd22917629702cf1647dfe95041ae2dce9e72f8
Beverly Pham <beverly.pham@gmail.com>
2018-04-04 15:27:40 -0700

    fix filename extension

--- stock_portfolio/stock_portfolio/views/default.py
+++ stock_portfolio/stock_portfolio/views/default.py
@@ -2,7 +2,22 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='home', renderer='../templates/index.jinja2', request_method='GET')
+def get_home_view(request):
+    return {}
 
+
+@view_config(route_name='auth', renderer='../templates/login.jinja2', request_method='GET')
+def get_auth_view(request):
+    return {}
+
+
+@view_config(route_name='stock', renderer='../templates/stock-add.jinja2', request_method='GET')
+def get_stock_view(request):
+    return {}
+
+
+@view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2', request_method='GET')
+def get_portfolio_view(request):
+    return {}
 
