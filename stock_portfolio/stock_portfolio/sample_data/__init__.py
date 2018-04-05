3a51f5f8ddecc84faf591df08771308f373a6344
Beverly Pham <beverly.pham@gmail.com>
2018-04-05 14:56:07 -0700

    make sample data

---
+++ stock_portfolio/stock_portfolio/sample_data/__init__.py
@@ -0,0 +1,113 @@
+MOCK_DATA = [
+{
+    "symbol": "GE",
+    "companyName": "General Electric Company",
+    "exchange": "New York Stock Exchange",
+    "industry": "Industrial Products",
+    "website": "http://www.ge.com",
+    "description": "General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "AAPL",
+    "companyName": "Apple Inc.",
+    "exchange": "Nasdaq Global Select",
+    "industry": "Industrial Products",
+    "website": "http://www.ge.com",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt feugiat nisi, at faucibus orci dignissim id. ",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "FB",
+    "companyName": "Facebook Inc.",
+    "exchange": "Nasdaq Global Select",
+    "industry": "Social Media",
+    "website": "http://www.facebook.com",
+    "description": "Duis tristique, urna sit amet semper elementum, ligula neque auctor magna, et feugiat libero nisl at ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "CNET",
+    "companyName": "ChinaNet Online Holdings Inc.",
+    "exchange": "NASDAQ Capital Market",
+    "industry": "Industrial Products",
+    "website": "http://www.cnet.com",
+    "description": "Phasellus in leo justo. Cras vulputate vel odio et rutrum. Vestibulum id magna volutpat, pharetra eros sed, tristique dui.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "MU",
+    "companyName": "Micron Technology Inc.",
+    "exchange": "Nasdaq Global Select",
+    "industry": "Industrial Products",
+    "website": "http://www.mu.com",
+    "description": "Quisque sit amet arcu interdum, semper massa lobortis, tempus mi. Ut quis nulla dolor. Sed sollicitudin enim at lacus gravida tempus. In vehicula, orci sed gravida rhoncus, libero mauris laoreet massa, in gravida dui sem vitae augue.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "AMD",
+    "companyName": "Advanced Micro Devices Inc.",
+    "exchange": "NASDAQ Capital Market",
+    "industry": "Industrial Products",
+    "website": "http://www.amd.com",
+    "description": "Cras libero ipsum, interdum eu mi id, laoreet sollicitudin sem. Cras ut felis metus.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "BAC",
+    "companyName": "Bank of America Corporation",
+    "exchange": "New York Stock Exchange",
+    "industry": "Industrial Products",
+    "website": "http://www.bac.com",
+    "description": "Cras lobortis, tellus nec sollicitudin auctor, orci purus fringilla elit, a porttitor est urna sed lacus. Suspendisse ac sagittis sapien. ",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "CZR",
+    "companyName": "Caesars Entertainment Corporation",
+    "exchange": "Nasdaq Global Select",
+    "industry": "Industrial Products",
+    "website": "http://www.czr.com",
+    "description": "Maecenas ex mauris, malesuada id sapien at, vulputate rutrum sapien. Donec condimentum ex vitae risus bibendum sodales.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "WFT",
+    "companyName": "Weatherford International plc (Ireland)",
+    "exchange": "New York Stock Exchange",
+    "industry": "Industrial Products",
+    "website": "http://www.wft.com",
+    "description": "Donec sit amet lacinia justo. Donec quis malesuada nisi. ",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+{
+    "symbol": "CHK",
+    "companyName": "Chesapeake Energy Corporation",
+    "exchange": "New York Stock Exchange",
+    "industry": "Industrial Products",
+    "website": "http://www.chk.com",
+    "description": "Sed at convallis mauris. Nunc vestibulum malesuada elit nec dapibus. Donec non porta ligula. Sed in erat a sem lobortis varius.",
+    "CEO": "John L. Flannery",
+    "issueType": "cs",
+    "sector": "Industrials"
+},
+
+]
