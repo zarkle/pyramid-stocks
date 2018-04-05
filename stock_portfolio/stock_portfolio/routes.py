8d6ec5987dc402e763d64f3488010ea91d455e19
Beverly Pham <beverly.pham@gmail.com>
2018-04-05 16:46:17 -0700

    portfolio view working

--- stock_portfolio/stock_portfolio/routes.py
+++ stock_portfolio/stock_portfolio/routes.py
@@ -2,6 +2,6 @@ def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('auth', '/auth')
-    config.add_route('stock', '/stock')
     config.add_route('portfolio', '/portfolio')
+    config.add_route('detail', '/detail')
     # config.add_route('portfolio/{}', '/portfolio/{}')
--- stock_portfolio/stock_portfolio/views/default.py
+++ stock_portfolio/stock_portfolio/views/default.py
@@ -1,23 +1,49 @@
-from pyramid.response import Response
 from pyramid.view import view_config
+from pyramid.response import Response
+from ..sample_data import MOCK_DATA
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 
 
-@view_config(route_name='home', renderer='../templates/index.jinja2', request_method='GET')
+@view_config(route_name='home', renderer='../templates/base.jinja2', request_method='GET')
 def get_home_view(request):
     return {}
 
 
-@view_config(route_name='auth', renderer='../templates/login.jinja2', request_method='GET')
-def get_auth_view(request):
-    return {}
+@view_config(route_name='auth', renderer='../templates/auth.jinja2')
+def auth_view(request):
+    if request.method == 'GET':
+        try:
+            username = request.GET['username']
+            password = request.GET['password']
+            print('User: {}, Pass: {}'.format(username, password))
 
+            return HTTPFound(location=request.route_url('portfolio'))
 
-@view_config(route_name='stock', renderer='../templates/stock-add.jinja2', request_method='GET')
-def get_stock_view(request):
-    return {}
+        except KeyError:
+            return {}
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        print('User: {}, Pass: {}, Email: {}'.format(username, password, email))
+
+        return HTTPFound(location=request.route_url('portfolio'))
 
 
 @view_config(route_name='portfolio', renderer='../templates/portfolio.jinja2', request_method='GET')
 def get_portfolio_view(request):
+    return {
+        'stocks': MOCK_DATA
+    }
+
+
+@view_config(route_name='detail', renderer='../templates/stock-detail.jinja2', request_method='GET')
+def get_detail_view(request):
     return {}
+
+
+# @view_config(route_name='add', renderer='../templates/stock-add.jinja2')
+# def add_view(request):
+#     return {}
 
