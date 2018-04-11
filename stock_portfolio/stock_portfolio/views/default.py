16c133861ff1d5bd05c89198f92b7a9a9ef10b83
Beverly Pham <beverly.pham@gmail.com>
2018-04-10 20:36:33 -0700

    make home view public

--- stock_portfolio/stock_portfolio/views/default.py
+++ stock_portfolio/stock_portfolio/views/default.py
@@ -1,7 +1,14 @@
 from pyramid.view import view_config
+from pyramid.security import NO_PERMISSION_REQUIRED, remember, forget
 
 
-@view_config(route_name='home', renderer='../templates/index.jinja2', request_method='GET')
+
+@view_config(
+    route_name='home',
+    renderer='../templates/index.jinja2',
+    request_method='GET',
+    permission=NO_PERMISSION_REQUIRED
+    )
 def home_view(request):
     """home view"""
     return {}
